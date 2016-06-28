5229eeed4f8fdf24c44a0acfcb43fbf12ebd4d76
jazzwhistle <jdcraftmcpe@gmail.com>
2016-06-29 01:10:21 +0200

    bugfix smallcase names

--- src/JD/RankPoints/Main.php
+++ src/JD/RankPoints/Main.php
@@ -13,30 +13,29 @@ use pocketmine\utils\Config;
 
 class Main extends PluginBase {
 
-    
     /** @var \_64FF00\PurePerms\PurePerms $purePerms */
     private $purePerms;
     private $ranksConfig;
-    
+
     public function onEnable() {
-        
-    $this->purePerms = $this->getServer()->getPluginManager()->getPlugin("PurePerms");
-    
+
+        $this->purePerms = $this->getServer()->getPluginManager()->getPlugin("PurePerms");
+
         if (!file_exists($this->getDataFolder() . "config.yml")) {
             @mkdir($this->getDataFolder());
             file_put_contents($this->getDataFolder() . "config.yml", $this->getResource("config.yml"));
         }
-        
-    $this->ranksConfig = yaml_parse(file_get_contents($this->getDataFolder() . "config.yml"));
-    
-    $num = 0;
-    foreach ($this->ranksConfig["Ranks"] as $i) {
-        echo $i . " - " . $this->ranksConfig["Points"][$num] . "\n";
-        $num ++;
+
+        $this->ranksConfig = yaml_parse(file_get_contents($this->getDataFolder() . "config.yml"));
+
+        $num = 0;
+        foreach ($this->ranksConfig["Ranks"] as $i) {
+            echo $i . " - " . $this->ranksConfig["Points"][$num] . "\n";
+            $num ++;
         }
     }
 
-        public function playerRegistered($playersname) {
+    public function playerRegistered($playersname) {
         $name = trim(strtolower($playersname));
         return file_exists($this->getDataFolder() . "players/" . $name . ".yml");
     }
@@ -65,26 +64,32 @@ class Main extends PluginBase {
     }
 
     public function onCommand(CommandSender $sender, Command $cmd, $label, array $args) {
-	
-        if (isset($args[0])) {$name = $args[0];}
-        if (isset($args[1])) {$pointstogive = $args[1];}
-        
-        if (isset($name) && strtolower($name) === "help"){
-        $sender->sendMessage("Rank up by voting on these sites:");
-        $sender->sendMessage("minecraftlist.org");
-        $sender->sendMessage("minecraftpocket-servers.com");
-        return true;
+
+        if (isset($args[0])) {
+            $name = $args[0];
         }
-        
+        if (isset($args[1])) {
+            $pointstogive = $args[1];
+        }
+
+        if (isset($name) && strtolower($name) === "help") {
+            $sender->sendMessage("Rank up by voting on these sites:");
+            $sender->sendMessage("minecraftlist.org");
+            $sender->sendMessage("minecraftpocket-servers.com");
+            return true;
+        }
+
         if ($sender instanceof Player) {
-            
-            if (!$this->playerRegistered($sender->getName()))  {$this->registerPlayer($sender->getName());}
+
+            if (!$this->playerRegistered($sender->getName())) {
+                $this->registerPlayer($sender->getName());
+            }
 
             if (isset($name)) {//Get rank points in game for a players name
                 $config = new Config($this->getDataFolder() . "players/" . strtolower($name) . ".yml", Config::YAML);
                 $data = $config->getAll();
-                
-                if (!isset($data["votes"])){
+
+                if (!isset($data["votes"])) {
                     $sender->sendMessage("That player never voted");
                 }
                 $votecount = $data["votes"];
@@ -99,63 +104,64 @@ class Main extends PluginBase {
 
             return true;
         }
- 
-        
 
-            
-            if (!isset($pointstogive)){
-                if (!isset($name)) {
-                   $sender->sendMessage("Type rankpoints playersname to show Rank Points for a player");
-                   return true;
-                }
-                
-                 if (!$this->playerRegistered($name)) {
-                 $sender->sendMessage("That player has never voted");
-                return true;
-            
-                }
-                
-                $config = new Config($this->getDataFolder() . "players/" . strtolower($name) . ".yml", Config::YAML);
-                
-                $data = $config->getAll();
-                
-                $votecount = $data["votes"];
-                $sender->sendMessage($name . " has a total of " . $votecount . " Rank Points");
-                     
+
+
+
+        if (!isset($pointstogive)) {
+            if (!isset($name)) {
+                $sender->sendMessage("Type rankpoints playersname to show Rank Points for a player");
                 return true;
             }
-            
+
             if (!$this->playerRegistered($name)) {
-                $this->registerPlayer($name);
+                $sender->sendMessage("That player has never voted");
+                return true;
             }
 
-            $p = $this->getServer()->getPlayer($name);
-            
-            if ($p == null ){
-        echo "Player is not online" . "\n";
+            $config = new Config($this->getDataFolder() . "players/" . strtolower($name) . ".yml", Config::YAML);
+
+            $data = $config->getAll();
+
+            $votecount = $data["votes"];
+            $sender->sendMessage($name . " has a total of " . $votecount . " Rank Points");
+
             return true;
-            }
-            
-                
-            $data = $this->getPlayerData($name);
-            $newvotes = $data["votes"] + $pointstogive;
+        }
 
-            $config = new Config($this->getDataFolder() . "players/" . $name . ".yml", Config::YAML);
-            $config->set("votes", $newvotes);
-            $config->save();
-            
-            $currentgroup = $this->purePerms->getUserDataMgr()->getGroup($p);
-            $currentgroupName = $currentgroup->getName();
-            $currentRankIndex = array_search($currentgroupName, $this->ranksConfig["Ranks"]);
-            if (!$currentRankIndex) return true;
-             
-            $num = 0;
-            foreach ($this->ranksConfig["Ranks"] as $i) {
-             
-                if ($newvotes >= $this->ranksConfig["Points"][$num]) {
+        if (!$this->playerRegistered($name)) {
+            $this->registerPlayer($name);
+        }
+
+        $p = $this->getServer()->getPlayer($name);
+
+        if (!isset($p)) {
+            echo "Player is not online" . "\n";
+            return true;
+        }
+
+
+        $data = $this->getPlayerData($name);
+        if (isset($data["votes"])) {$oldvotes = $data["votes"];} else {$oldvotes = 0;}
+        $newvotes = $oldvotes + $pointstogive;
+
+        $config = new Config($this->getDataFolder() . "players/" . strtolower($name) . ".yml", Config::YAML);
+        $config->set("votes", $newvotes);
+        $config->save();
+
+        $currentgroup = $this->purePerms->getUserDataMgr()->getGroup($p);
+        $currentgroupName = $currentgroup->getName();
+        $currentRankIndex = array_search($currentgroupName, $this->ranksConfig["Ranks"]);
+        if (!$currentRankIndex)
+            return true;
+
+        $num = 0;
+        foreach ($this->ranksConfig["Ranks"] as $i) {
+
+            if ($newvotes >= $this->ranksConfig["Points"][$num]) {
 
                 $configRankIndex = array_search($i, $this->ranksConfig["Ranks"]);
-                
+
                 if ($currentRankIndex < $configRankIndex) {
                     $newgroup = $this->purePerms->getGroup($i);
                     $this->purePerms->getUserDataMgr()->setGroup($p, $newgroup, null);
@@ -163,14 +169,11 @@ class Main extends PluginBase {
                 }
             }
             $num ++;
-                
         }
-        
-            
+
+
         return true;
     }
-
-
 
 }
 
